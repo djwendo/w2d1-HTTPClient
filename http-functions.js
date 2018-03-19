f2723f7d0dcf9073b63e6641ad4521b2c72faf69
Wendy Braun <wendocalrissian@gmail.com>
2018-03-19 20:21:48 +0000

    Adding http-functions.js and testing.js files - Step 5: Making function modular

---
+++ http-functions.js
@@ -0,0 +1,16 @@
+var https = require('https');
+
+module.exports = function getHTML (options, callback) {
+
+    var incomingData = '';
+
+    https.get(options, function (response) {
+      response.setEncoding('utf8');
+      response.on('data', function (data) {
+        incomingData += data;
+      });
+      response.on('end', function() {
+        callback(incomingData);
+      });
+    });
+};
